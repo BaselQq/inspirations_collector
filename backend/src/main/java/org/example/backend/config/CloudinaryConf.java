6b2316f926bf1756345e6291b57612ce5a40810a
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-06-04 05:10:09 +0200

    fix cloudinary config

--- backend/src/main/java/org/example/backend/config/CloudinaryConf.java
+++ backend/src/main/java/org/example/backend/config/CloudinaryConf.java
@@ -11,13 +11,13 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConf {
 
-    @Value("${CLOUDINARY_CLOUD_NAME}")
+    @Value("${CLOUDINARY_CLOUD_NAME:default_cloud_name}")
     private String cloudName;
 
-    @Value("${CLOUDINARY_API_KEY}")
+    @Value("${CLOUDINARY_API_KEY:default_api_key}")
     private String apiKey;
 
-    @Value("${CLOUDINARY_API_SEC}")
+    @Value("${CLOUDINARY_API_SECRET:default_api_secret}")
     private String apiSecret;
 
     @Bean
