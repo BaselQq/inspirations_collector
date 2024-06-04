5cfe5315883503a92b2783aebe106d0497e4a4e7
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-06-04 02:28:55 +0200

    fix cloudinary tests

--- backend/src/main/java/org/example/backend/config/CloudinaryConf.java
+++ backend/src/main/java/org/example/backend/config/CloudinaryConf.java
@@ -5,29 +5,27 @@ import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.util.Map;
 
 @Configuration
-@Profile("!test")
 public class CloudinaryConf {
 
-    @Value("${CLOUDINARY_CLOUD_NAME}")
+    @Value("${CLOUDINARY_CLOUD_NAME:default_cloud_name}")
     private String cloudName;
 
-    @Value("${CLOUDINARY_API_KEY}")
+    @Value("${CLOUDINARY_API_KEY:default_api_key}")
     private String apiKey;
 
-    @Value("${CLOUDINARY_API_SEC}")
-    private String apiSec;
+    @Value("${CLOUDINARY_API_SECRET:default_api_secret}")
+    private String apiSecret;
 
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = ObjectUtils.asMap(
                 "cloud_name", cloudName,
                 "api_key", apiKey,
-                "api_secret", apiSec
+                "api_secret", apiSecret
         );
         return new Cloudinary(config);
     }
