89ee3462f6b34abcf3b177326e5de8fa7354d4ee
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-06-07 16:47:15 +0200

    add initial login with github

---
+++ backend/src/main/java/org/example/backend/config/SecurityConf.java
@@ -0,0 +1,34 @@
+package org.example.backend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConf {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers(HttpMethod.DELETE, "/api/*").authenticated()
+                        .anyRequest().permitAll())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .oauth2Login(withDefaults());
+
+                return http.build();
+    }
+}
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { Container, Group, Button, Text, Title } from '@mantine/core';
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 const Header: React.FC = () => {
+
+    function login(){
+        const host = window.location.host == 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+
+        window.open(host + "/oauth2/authorization/github", "_self")
+    }
+
+    function getUser() {
+        axios.get("http://localhost:8080/api/users/me")
+            .then(response => {
+                console.log(response.data)
+            })
+    }
+
     return (
         <Container>
             <Group position="apart" spacing="xs" align="center">
@@ -14,8 +29,9 @@ const Header: React.FC = () => {
                         <Button variant="default">Sign Up</Button>
                     </Link>
                     <Link to="/signin">
-                        <Button variant="default">Sign In</Button>
+                        <Button variant="default" onClick={login}>Sign In</Button>
                     </Link>
+                    <Button variant="default" onClick={getUser}>Me</Button>
                 </Group>
             </Group>
         </Container>
